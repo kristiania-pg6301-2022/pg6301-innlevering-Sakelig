4ce4374cbfc85453c595be98d0b9d20acce910a0
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-02-20 00:47:24 +0100

    husky test

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>Helloooo</h1>, document.getElementById("app"))
+ReactDOM.render(<h1>Helloodddddoo</h1>, document.getElementById("app"))
