c9e5e265ebb796ed9f30ff33b7d57fb71d069f29
Sakelig <71970061+Sakelig@users.noreply.github.com>
2022-02-20 18:48:48 +0100

    test

--- client/QuizApplication.jsx
+++ client/QuizApplication.jsx
@@ -1,36 +1,20 @@
 import * as React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Quiz from "./Quiz";
-import {FrontPage} from "./FrontPage";
-import {ShowAnswer} from "./ShowAnswer";
-
+import { FrontPage } from "./FrontPage";
+import { ShowAnswer } from "./ShowAnswer";
 
 export function QuizApplication() {
-
-    return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path={"/"}
-                        element={
-                            <FrontPage
-
-                            />
-                        }
-                    />
-                    <Route
-                        path={"/question"}
-                        element={
-                            <Quiz
-
-                            />
-                        }
-                    />
-                    <Route path={"/answer/*"} element={<ShowAnswer />} />
-                    <Route path={"*"} element={<h1>Not Found</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<FrontPage />} />
+          <Route path={"/question"} element={<Quiz />} />
+          <Route path={"/answer/*"} element={<ShowAnswer />} />
+          <Route path={"*"} element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
